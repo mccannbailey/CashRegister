8e3acf80f4842550d8da4bb19cfba3027844eec4
Bailey <bailmcca601@ed.amdsb.ca>
2016-10-13 12:25:00 -0400

    added calculate totals

--- cash register/Form1.Designer.cs
+++ cash register/Form1.Designer.cs
@@ -28,12 +28,322 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.titlelabel = new System.Windows.Forms.Label();
+            this.whiteshirtlabel = new System.Windows.Forms.Label();
+            this.baggyjeanslabel = new System.Windows.Forms.Label();
+            this.bluebandanalabel = new System.Windows.Forms.Label();
+            this.timbslabel = new System.Windows.Forms.Label();
+            this.shirtbutton = new System.Windows.Forms.Button();
+            this.jeanbutton = new System.Windows.Forms.Button();
+            this.timbsbutton = new System.Windows.Forms.Button();
+            this.bandanabutton = new System.Windows.Forms.Button();
+            this.apparelbox = new System.Windows.Forms.Label();
+            this.outputlabel1 = new System.Windows.Forms.Label();
+            this.outputlabel2 = new System.Windows.Forms.Label();
+            this.outputlabel3 = new System.Windows.Forms.Label();
+            this.outputlabel4 = new System.Windows.Forms.Label();
+            this.outputlabel5 = new System.Windows.Forms.Label();
+            this.totalbutton = new System.Windows.Forms.Button();
+            this.subtotallabel = new System.Windows.Forms.Label();
+            this.taxlabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grandtotallabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titlelabel
+            // 
+            this.titlelabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.titlelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titlelabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.titlelabel.Location = new System.Drawing.Point(-1, 9);
+            this.titlelabel.Name = "titlelabel";
+            this.titlelabel.Size = new System.Drawing.Size(519, 41);
+            this.titlelabel.TabIndex = 0;
+            this.titlelabel.Text = "The Crip Walk";
+            this.titlelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // whiteshirtlabel
+            // 
+            this.whiteshirtlabel.AutoSize = true;
+            this.whiteshirtlabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.whiteshirtlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.whiteshirtlabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.whiteshirtlabel.Location = new System.Drawing.Point(2, 104);
+            this.whiteshirtlabel.Name = "whiteshirtlabel";
+            this.whiteshirtlabel.Size = new System.Drawing.Size(177, 16);
+            this.whiteshirtlabel.TabIndex = 2;
+            this.whiteshirtlabel.Text = "$5.00 - White T-Shirt (XL)";
+            // 
+            // baggyjeanslabel
+            // 
+            this.baggyjeanslabel.AutoSize = true;
+            this.baggyjeanslabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.baggyjeanslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.baggyjeanslabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.baggyjeanslabel.Location = new System.Drawing.Point(2, 145);
+            this.baggyjeanslabel.Name = "baggyjeanslabel";
+            this.baggyjeanslabel.Size = new System.Drawing.Size(156, 16);
+            this.baggyjeanslabel.TabIndex = 3;
+            this.baggyjeanslabel.Text = "$10.50 - Baggy Jeans";
+            // 
+            // bluebandanalabel
+            // 
+            this.bluebandanalabel.AutoSize = true;
+            this.bluebandanalabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.bluebandanalabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bluebandanalabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bluebandanalabel.Location = new System.Drawing.Point(2, 187);
+            this.bluebandanalabel.Name = "bluebandanalabel";
+            this.bluebandanalabel.Size = new System.Drawing.Size(154, 16);
+            this.bluebandanalabel.TabIndex = 4;
+            this.bluebandanalabel.Text = "$2.50 - Blue Bandana";
+            // 
+            // timbslabel
+            // 
+            this.timbslabel.AutoSize = true;
+            this.timbslabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.timbslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timbslabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.timbslabel.Location = new System.Drawing.Point(2, 229);
+            this.timbslabel.Name = "timbslabel";
+            this.timbslabel.Size = new System.Drawing.Size(188, 16);
+            this.timbslabel.TabIndex = 5;
+            this.timbslabel.Text = "$100 -Timb\'s Wheat Boots";
+            // 
+            // shirtbutton
+            // 
+            this.shirtbutton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.shirtbutton.Location = new System.Drawing.Point(205, 101);
+            this.shirtbutton.Name = "shirtbutton";
+            this.shirtbutton.Size = new System.Drawing.Size(84, 23);
+            this.shirtbutton.TabIndex = 6;
+            this.shirtbutton.Text = "Add to Cart";
+            this.shirtbutton.UseVisualStyleBackColor = true;
+            this.shirtbutton.Click += new System.EventHandler(this.shirtbutton_Click);
+            // 
+            // jeanbutton
+            // 
+            this.jeanbutton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.jeanbutton.Location = new System.Drawing.Point(205, 140);
+            this.jeanbutton.Name = "jeanbutton";
+            this.jeanbutton.Size = new System.Drawing.Size(84, 23);
+            this.jeanbutton.TabIndex = 7;
+            this.jeanbutton.Text = "Add to Cart";
+            this.jeanbutton.UseVisualStyleBackColor = true;
+            this.jeanbutton.Click += new System.EventHandler(this.jeanbutton_Click);
+            // 
+            // timbsbutton
+            // 
+            this.timbsbutton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.timbsbutton.Location = new System.Drawing.Point(205, 222);
+            this.timbsbutton.Name = "timbsbutton";
+            this.timbsbutton.Size = new System.Drawing.Size(84, 23);
+            this.timbsbutton.TabIndex = 8;
+            this.timbsbutton.Text = "Add to Cart";
+            this.timbsbutton.UseVisualStyleBackColor = true;
+            this.timbsbutton.Click += new System.EventHandler(this.timbsbutton_Click);
+            // 
+            // bandanabutton
+            // 
+            this.bandanabutton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.bandanabutton.Location = new System.Drawing.Point(205, 180);
+            this.bandanabutton.Name = "bandanabutton";
+            this.bandanabutton.Size = new System.Drawing.Size(84, 23);
+            this.bandanabutton.TabIndex = 9;
+            this.bandanabutton.Text = "Add to Cart";
+            this.bandanabutton.UseVisualStyleBackColor = true;
+            this.bandanabutton.Click += new System.EventHandler(this.bandanabutton_Click);
+            // 
+            // apparelbox
+            // 
+            this.apparelbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.apparelbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.apparelbox.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.apparelbox.Location = new System.Drawing.Point(0, 60);
+            this.apparelbox.Name = "apparelbox";
+            this.apparelbox.Size = new System.Drawing.Size(305, 209);
+            this.apparelbox.TabIndex = 1;
+            this.apparelbox.Text = "Apparel";
+            this.apparelbox.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // outputlabel1
+            // 
+            this.outputlabel1.AutoSize = true;
+            this.outputlabel1.Location = new System.Drawing.Point(334, 106);
+            this.outputlabel1.Name = "outputlabel1";
+            this.outputlabel1.Size = new System.Drawing.Size(0, 13);
+            this.outputlabel1.TabIndex = 12;
+            // 
+            // outputlabel2
+            // 
+            this.outputlabel2.AutoSize = true;
+            this.outputlabel2.Location = new System.Drawing.Point(334, 145);
+            this.outputlabel2.Name = "outputlabel2";
+            this.outputlabel2.Size = new System.Drawing.Size(0, 13);
+            this.outputlabel2.TabIndex = 13;
+            // 
+            // outputlabel3
+            // 
+            this.outputlabel3.AutoSize = true;
+            this.outputlabel3.Location = new System.Drawing.Point(334, 189);
+            this.outputlabel3.Name = "outputlabel3";
+            this.outputlabel3.Size = new System.Drawing.Size(0, 13);
+            this.outputlabel3.TabIndex = 14;
+            // 
+            // outputlabel4
+            // 
+            this.outputlabel4.AutoSize = true;
+            this.outputlabel4.Location = new System.Drawing.Point(334, 227);
+            this.outputlabel4.Name = "outputlabel4";
+            this.outputlabel4.Size = new System.Drawing.Size(0, 13);
+            this.outputlabel4.TabIndex = 15;
+            // 
+            // outputlabel5
+            // 
+            this.outputlabel5.AutoSize = true;
+            this.outputlabel5.Location = new System.Drawing.Point(305, 278);
+            this.outputlabel5.Name = "outputlabel5";
+            this.outputlabel5.Size = new System.Drawing.Size(0, 13);
+            this.outputlabel5.TabIndex = 16;
+            // 
+            // totalbutton
+            // 
+            this.totalbutton.Location = new System.Drawing.Point(324, 268);
+            this.totalbutton.Name = "totalbutton";
+            this.totalbutton.Size = new System.Drawing.Size(168, 23);
+            this.totalbutton.TabIndex = 18;
+            this.totalbutton.Text = "Calculate Totals";
+            this.totalbutton.UseVisualStyleBackColor = true;
+            this.totalbutton.Click += new System.EventHandler(this.totalbutton_Click);
+            // 
+            // subtotallabel
+            // 
+            this.subtotallabel.AutoSize = true;
+            this.subtotallabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotallabel.Location = new System.Drawing.Point(334, 307);
+            this.subtotallabel.Name = "subtotallabel";
+            this.subtotallabel.Size = new System.Drawing.Size(68, 15);
+            this.subtotallabel.TabIndex = 19;
+            this.subtotallabel.Text = "Subtotal: ";
+            // 
+            // taxlabel
+            // 
+            this.taxlabel.AutoSize = true;
+            this.taxlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxlabel.Location = new System.Drawing.Point(334, 330);
+            this.taxlabel.Name = "taxlabel";
+            this.taxlabel.Size = new System.Drawing.Size(34, 15);
+            this.taxlabel.TabIndex = 20;
+            this.taxlabel.Text = "Tax:";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(495, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 341);
+            this.label1.TabIndex = 21;
+            // 
+            // grandtotallabel
+            // 
+            this.grandtotallabel.AutoSize = true;
+            this.grandtotallabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grandtotallabel.Location = new System.Drawing.Point(334, 353);
+            this.grandtotallabel.Name = "grandtotallabel";
+            this.grandtotallabel.Size = new System.Drawing.Size(86, 15);
+            this.grandtotallabel.TabIndex = 22;
+            this.grandtotallabel.Text = "Grand Total:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 284);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 20);
+            this.textBox1.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(137, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Tender:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(139, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(733, 388);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.grandtotallabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.taxlabel);
+            this.Controls.Add(this.subtotallabel);
+            this.Controls.Add(this.totalbutton);
+            this.Controls.Add(this.outputlabel5);
+            this.Controls.Add(this.outputlabel4);
+            this.Controls.Add(this.outputlabel3);
+            this.Controls.Add(this.outputlabel2);
+            this.Controls.Add(this.outputlabel1);
+            this.Controls.Add(this.bandanabutton);
+            this.Controls.Add(this.timbsbutton);
+            this.Controls.Add(this.jeanbutton);
+            this.Controls.Add(this.shirtbutton);
+            this.Controls.Add(this.timbslabel);
+            this.Controls.Add(this.bluebandanalabel);
+            this.Controls.Add(this.baggyjeanslabel);
+            this.Controls.Add(this.whiteshirtlabel);
+            this.Controls.Add(this.apparelbox);
+            this.Controls.Add(this.titlelabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titlelabel;
+        private System.Windows.Forms.Label whiteshirtlabel;
+        private System.Windows.Forms.Label baggyjeanslabel;
+        private System.Windows.Forms.Label bluebandanalabel;
+        private System.Windows.Forms.Label timbslabel;
+        private System.Windows.Forms.Button shirtbutton;
+        private System.Windows.Forms.Button jeanbutton;
+        private System.Windows.Forms.Button timbsbutton;
+        private System.Windows.Forms.Button bandanabutton;
+        private System.Windows.Forms.Label apparelbox;
+        private System.Windows.Forms.Label outputlabel1;
+        private System.Windows.Forms.Label outputlabel2;
+        private System.Windows.Forms.Label outputlabel3;
+        private System.Windows.Forms.Label outputlabel4;
+        private System.Windows.Forms.Label outputlabel5;
+        private System.Windows.Forms.Button totalbutton;
+        private System.Windows.Forms.Label subtotallabel;
+        private System.Windows.Forms.Label taxlabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label grandtotallabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- cash register/Form1.cs
+++ cash register/Form1.cs
@@ -12,9 +12,62 @@ namespace cash_register
 {
     public partial class Form1 : Form
     {
+        const double whiteshirt = 5;
+        const double baggyjeans = 10.5;
+        const double bandana = 2.5;
+        const double timbs = 100;
+        const double tax = 1.13;
+        const double taxmultiplier = 0.13;
+        int shirtcounter = 0;
+        int jeancounter = 0;
+        int bandanacounter = 0;
+        int timbscounter = 0;
+
         public Form1()
         {
-            InitializeComponent();
+        InitializeComponent();    
+        }
+        private void shirtbutton_Click(object sender, EventArgs e)
+        {
+            shirtcounter++;
+            outputlabel1.Text = "White T-Shirt (XL) x" + shirtcounter;
+        }
+        private void jeanbutton_Click(object sender, EventArgs e)
+        {
+            jeancounter++;
+            outputlabel2.Text = "Baggy Jean x" + jeancounter;
+        }
+
+        private void bandanabutton_Click(object sender, EventArgs e)
+        {
+            bandanacounter++;
+            outputlabel3.Text = "Bandana x" + bandanacounter;
+        }
+
+        private void timbsbutton_Click(object sender, EventArgs e)
+        {
+            timbscounter++;
+            outputlabel4.Text = "Timbs Boots x" + timbscounter;
+        }
+        private void totalbutton_Click(object sender, EventArgs e)
+        {
+            double shirtprice = shirtcounter * 5;
+            double jeanprice = jeancounter * 10.5;
+            double bandanaprice = bandanacounter * 2.5;
+            double timbsprice = timbscounter * 100;
+
+            double subtotaldouble = shirtprice + jeanprice + bandanaprice + timbsprice;
+            string subtotal = subtotaldouble.ToString("C");
+            subtotallabel.Text = "Subtotal: " + subtotal;
+
+            double taxbefore = subtotaldouble * taxmultiplier;
+            string taxalone = taxbefore.ToString("C");
+            taxlabel.Text = "Tax: " + taxalone;
+
+            double total = subtotaldouble * tax;
+            string grandtotal = total.ToString("C");
+            grandtotallabel.Text = "Grand Total: " + grandtotal;
         }
     }
 }
+
